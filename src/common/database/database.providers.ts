664fd9797ef1c132c7b09c652a6435ca80592a44
Srdjan Zupljanin <srdjan.zupljanin@jsguru.io>
2021-11-19 16:24:40 +0100

    decouple the providers file, introduce separate factories

--- src/common/database/database.providers.ts
+++ src/common/database/database.providers.ts
@@ -1,54 +1,17 @@
-import { Sequelize } from 'sequelize-typescript';
 import { Provider } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { MIGRATOR_TOKEN, SEQUELIZE_TOKEN } from './const';
-import { SequelizeStorage, Umzug } from 'umzug';
+import { sequelizeFactory, migratorFactory } from './factory';
 
 export const databaseProviders: Provider[] = [
   {
     provide: SEQUELIZE_TOKEN,
-    useFactory: async (configService: ConfigService): Promise<Sequelize> => {
-      return new Sequelize({
-        dialect: 'postgres',
-        host: configService.get<string>('DB_HOST'),
-        port: +configService.get<number>('DB_PORT'),
-        username: configService.get<string>('DB_USER'),
-        password: configService.get<string>('DB_PASSWORD'),
-        database: configService.get<string>('DB_NAME'),
-        models: [`${__dirname}/../../**/*.model.{ts,js}`],
-        modelMatch: (filename: string, member) => {
-          const exportedMember: string = filename.substring(
-            0,
-            filename.indexOf('.model'),
-          );
-          return exportedMember === member.toLowerCase();
-        },
-      });
-    },
+    useFactory: sequelizeFactory,
     inject: [ConfigService],
   },
   {
     provide: MIGRATOR_TOKEN,
-    useFactory: async (sequelize: Sequelize) => {
-      const migrator: Umzug = new Umzug({
-        migrations: {
-          glob: [
-            '**/migrations/*.migration.{ts,js}',
-            {
-              cwd: __dirname,
-            },
-          ],
-        },
-        context: sequelize.getQueryInterface(),
-        storage: new SequelizeStorage({
-          sequelize,
-        }),
-        logger: console,
-      });
-      await migrator.down();
-      await migrator.up();
-      return migrator;
-    },
+    useFactory: migratorFactory,
     inject: [SEQUELIZE_TOKEN],
   },
 ];
---
+++ src/common/database/factory/migrator.factory.ts
@@ -0,0 +1,24 @@
+import { Sequelize } from 'sequelize-typescript';
+import { SequelizeStorage, Umzug } from 'umzug';
+
+export type Migrator = Umzug;
+
+export const migratorFactory = async (
+  sequelize: Sequelize,
+): Promise<Migrator> => {
+  return new Umzug({
+    migrations: {
+      glob: [
+        '**/migrations/*.migration.{ts,js}',
+        {
+          cwd: __dirname,
+        },
+      ],
+    },
+    context: sequelize.getQueryInterface(),
+    storage: new SequelizeStorage({
+      sequelize,
+    }),
+    logger: console,
+  });
+};
