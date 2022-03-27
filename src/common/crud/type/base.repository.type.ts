ba6951498fbba6d047a1224f0f6262a75f2069a3
Srdjan Zupljanin <srdjan.zupljanin@jsguru.io>
2022-03-27 13:46:42 +0200

    update dependencies, update sequelize types

--- src/common/crud/type/base.repository.type.ts
+++ src/common/crud/type/base.repository.type.ts
@@ -1,10 +1,11 @@
 import { Model } from 'sequelize-typescript';
+import { CreationAttributes, Attributes } from 'sequelize/types/model';
 import { Constructor } from '../../util';
 
 export type ModelType<T extends Model<T>> = Constructor<T> & typeof Model;
 export type ModelPayload<T> = Partial<Omit<T, keyof Model<T>>>;
-export type ModelAttributes<T extends Model> = T['_attributes'];
-export type ModelCreationAttributes<T extends Model> = T['_creationAttributes'];
+export type ModelAttributes<T extends Model> = Attributes<T>;
+export type ModelCreationAttributes<T extends Model> = CreationAttributes<T>;
 export type ResultsWithCountSet<T extends Model> = {
   rows: T[];
   count: number;
